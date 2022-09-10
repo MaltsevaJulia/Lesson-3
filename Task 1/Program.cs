4adc65b1a635e81a404f9f7c03bc8428e24808df
Julia <Y650480@yandex.ru>
2022-09-10 20:08:38 +0300

    ДЗ

---
+++ Task 1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+
+    return value;
+}
+
+string ReverseString(string s)
+{
+    char[] arr = s.ToCharArray();
+    Array.Reverse(arr);
+    return new string(arr);
+}
+
+string str = Prompt("введите число");
+string strrev = ReverseString(str);
+
+if (str == strrev)
+{
+    Console.WriteLine("Это палиндром");
+}
+else
+{
+    Console.WriteLine("Это не палиндром");
+}
+
+
---
+++ Task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int Prompt(string message)
+{
+System.Console.Write(message);
+string value = Console.ReadLine();
+return Convert.ToInt32(value);
+}
+
+int N = Prompt ("ВВедите число :");
+for (int count=1; count <= N; count++)
+{
+int result = count*count*count;
+System.Console.WriteLine(result);
+}
